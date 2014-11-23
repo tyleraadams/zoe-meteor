4563abc6489a65371fa1bd2b3da21a764dac4556
Tyler Adams <tyleraadams@gmail.com>
2014-11-23 18:06:36 -0500

    init commit

---
+++ client/main.js
@@ -0,0 +1,30 @@
+var MainTemplate = (function(){
+
+	function onRender() {
+
+	}
+
+	function showFullArticle(e) {
+		
+		$(e.target).parent().parent().parent().find('.showOnClick').css('display', 'inline-block');
+		debugger
+		$(e.target).parent().parent().parent().click(function(e){
+			$(e.target).find('.showOnClick').css('display', 'none');
+		})
+
+	}
+
+
+	//public methods
+	return {
+		onRender: onRender,
+		showFullArticle: showFullArticle
+	}
+
+})();
+
+Template.main.rendered = MainTemplate.onRender;
+
+Template.main.events({
+	'click .preview': MainTemplate.showFullArticle
+});
