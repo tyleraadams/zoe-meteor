5af267d25030be2f40a102eed4c0154b41763039
Tyler Adams <tyleraadams@gmail.com>
2014-11-29 17:16:53 -0500

    reset form after submit

--- client/contact/contact.js
+++ client/contact/contact.js
@@ -2,15 +2,23 @@ var ContactTemplate = (function(){
 
 	function sendMessage(e) {
 		e.preventDefault();
-    debugger
+    
     var contact = {};
     contact.name = $(e.target).find('#name').val();
     contact.email = $(e.target).find('#email').val();
     contact.message = $(e.target).find('#message').val();
     Meteor.call('sendMessage', contact, function(err,result){
       console.log(result);
+      clearForm(e.target);
     });
 
+    
+
+	}
+
+	function clearForm(form){
+		
+		form.reset();
 	}
 
 
